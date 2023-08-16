2b2024b1e99317b6683b4f0151920d2051118b07
CaioMiranda12 <caioclm04@gmail.com>
2023-08-16 09:49:10 -0300

    first commit

---
+++ src/database/migrations/20230812005013-create-users.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false, // Para não permitir que fique em branco
+        primaryKey: true, // Para definir que o valor do ID não pode se repetir nunca
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true, // Para definir que o email é algo único
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users')
+  },
+}
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+
+import User from './app/models/User'
+
+import { v4 } from 'uuid'
+
+const routes = new Router()
+
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Caio',
+    email: 'caioclm02@gmail.com',
+    password_hash: 'h2s4e32d',
+  })
+  return response.json(user)
+})
+
+export default routes
